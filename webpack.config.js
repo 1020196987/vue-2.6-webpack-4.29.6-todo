7187192eb17a5802fcb88b0c0874b543e91b7c4b
pcm <1020196987@qq.com>
2019-03-19 09:46:54 +0800

    升级webpack4的mode模式

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,14 @@
 const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
-const webpack = require('webpack')
+// const webpack = require('webpack')
 const HTMLPlugin = require('html-webpack-plugin')
-const ExtractPlugin = require('extract-text-webpack-plugin')
-const isDev = process.env.NODE_ENV === 'development'
-
-const config = {
-    target: 'web',
+// const ExtractPlugin = require('extract-text-webpack-plugin')
+// const isDev = process.env.NODE_ENV === 'development'
+module.exports = {
+    mode: "development",
     entry: path.join(__dirname, 'src/index.js'),
     output: {
-        filename:'bundle.[hash:8].js',
+        filename: 'bundle.[hash:8].js',
         path: path.join(__dirname, 'dist')
     },
     // 配置webpack-dev-server
@@ -37,60 +36,11 @@ const config = {
                         }
                     }
                 ]
-            }
-        ]
-    },
-    plugins: [
-        new VueLoaderPlugin(),
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: isDev ? '"development"' : '"production"'
-            }
-        }),
-        new HTMLPlugin()
-    ]
-}
-// 如果是开发环境，则进行如下配置
-if (isDev) {
-    config.module.rules.push({
-        test: /\.styl/,
-        use: [
-            'style-loader',
-            'css-loader',
-            {
-                loader: 'postcss-loader',
-                options: {
-                    sourceMap: true,
-                }
             },
-            'stylus-loader'
-        ]
-    })
-    config.devtool = '#cheap-module-eval-source-map'
-    config.devServer = {
-        port: 8000,
-        host: '0.0.0.0',
-        overlay: {
-            errors: true,
-        },
-        hot: true
-    }
-    config.plugins.push(
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoEmitOnErrorsPlugin()
-    )
-} else {
-    config.entry = {
-        app: path.join(__dirname,'src/index.js'),
-        vendor: ['vue']
-    }
-    config.output.filename = '[name].[chunkhash:8].js'
-    config.module.rules.push(
-        {
-            test: /\.styl/,
-            use: ExtractPlugin.extract({
-                fallback: 'style-loader',
+            {
+                test: /\.styl/,
                 use: [
+                    'style-loader',
                     'css-loader',
                     {
                         loader: 'postcss-loader',
@@ -100,12 +50,76 @@ if (isDev) {
                     },
                     'stylus-loader'
                 ]
-            })
-        },
-    )
-    config.plugins.push(
-        new ExtractPlugin('style.[chunkhash:8].css'),
-    )
-}
+            },
 
-module.exports = config
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        // new webpack.DefinePlugin({
+        //     'process.env': {
+        //         NODE_ENV: isDev ? '"development"' : '"production"'
+        //     }
+        // }),
+        new HTMLPlugin()
+    ]
+}
+// 如果是开发环境，则进行如下配置
+// if (isDev) {
+//     config.module.rules.push({
+//         test: /\.styl/,
+//         use: [
+//             'style-loader',
+//             'css-loader',
+//             {
+//                 loader: 'postcss-loader',
+//                 options: {
+//                     sourceMap: true,
+//                 }
+//             },
+//             'stylus-loader'
+//         ]
+//     })
+//     config.devtool = '#cheap-module-eval-source-map'
+//     config.devServer = {
+//         port: 8000,
+//         host: '0.0.0.0',
+//         overlay: {
+//             errors: true,
+//         },
+//         hot: true
+//     }
+//     config.plugins.push(
+//         new webpack.HotModuleReplacementPlugin(),
+//         new webpack.NoEmitOnErrorsPlugin()
+//     )
+// } else {
+//     config.entry = {
+//         app: path.join(__dirname,'src/index.js'),
+//         vendor: ['vue']
+//     }
+//     config.output.filename = '[name].[chunkhash:8].js'
+//     config.module.rules.push(
+//         {
+//             test: /\.styl/,
+//             use: ExtractPlugin.extract({
+//                 fallback: 'style-loader',
+//                 use: [
+//                     'css-loader',
+//                     {
+//                         loader: 'postcss-loader',
+//                         options: {
+//                             sourceMap: true,
+//                         }
+//                     },
+//                     'stylus-loader'
+//                 ]
+//             })
+//         },
+//     )
+//     config.plugins.push(
+//         new ExtractPlugin('style.[chunkhash:8].css'),
+//     )
+// }
+//
+// module.exports = config
